bec3beb7b207715636dabc7bc6dae5ed48576a91
Zaphyrzan <wzafirzan@gmail.com>
2021-10-04 12:03:00 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 03</title>
+</head>
+<body>
+    <h1>Asas PHP</h1>
+
+    <?php
+    //komen satu baris
+    # Komen lagi satu baris
+    /*
+        Komen banyak baris
+    */
+
+    $nama = 'Wan Zafirzan';
+    echo "<h2>Hello $nama</h2>"; #output
+
+    $hadir = false;
+    $umur = 20;
+    $nokp = '010403081255'
+
+    ?>
+</body>
+</html>
